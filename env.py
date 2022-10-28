0659649109173285a7e0b5eccccb535389edfba8
SCollins <66878688+SJECollins@users.noreply.github.com>
2022-10-28 20:27:22 +0100

    Delete env.py
    Shouldn't be here!

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ['CLOUDINARY_URL'] = 'cloudinary://954251982836456:gJIJOzlNMRhBfmvQ-xuA2SpldCk@whatsit'
-os.environ['CLOUDINARY_CLOUD_NAME'] = 'whatsit'
-os.environ['CLOUDINARY_API_KEY'] = '954251982836456'
-os.environ['CLOUDINARY_API_SECRET'] = 'gJIJOzlNMRhBfmvQ-xuA2SpldCk'
