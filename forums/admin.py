030af3c8612ce56888a00c795a79dbfc11f559e1
SCollins <sje.collins@gmail.com>
2022-10-28 13:57:47 +0000

    Styling plant detail

--- forums/admin.py
+++ forums/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Forum
+from .models import Forum, Discussion, Post
 
 
 admin.site.register(Forum)
+admin.site.register(Discussion)
+admin.site.register(Post)
+
